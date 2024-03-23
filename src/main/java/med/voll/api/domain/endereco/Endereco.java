ee38b9afc9391f140ddbfd886b78da85e6faeee9
renato1006 <renato1006@gmail.com>
2024-03-22 21:28:26 -0300

    Reorganização dos arquivos nos packages, aplicação do tratamento de erros das requisições HTTP

--- src/main/java/med/voll/api/domain/endereco/Endereco.java
+++ src/main/java/med/voll/api/domain/endereco/Endereco.java
@@ -1,10 +1,10 @@
-package med.voll.api.end;
+package med.voll.api.domain.endereco;
 
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.domain.endereco.DadosEndereco;
 
 @Embeddable
 @Getter
