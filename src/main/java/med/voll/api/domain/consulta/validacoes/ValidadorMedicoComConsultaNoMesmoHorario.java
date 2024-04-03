eee101de29c141a50acba6f4a1a7000b8a7c066c
renato1006 <renato1006@gmail.com>
2024-04-02 22:23:44 -0300

    Create: implementando uma interface de validacão do agendamento, onde aplicamos principios do SOLID e implementamos todas as validações solicitadas.

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoComConsultaNoMesmoHorario.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.ConsultaRepository;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidadorMedicoComConsultaNoMesmoHorario implements ValidadorAgendamentoDeConsultas {
+    @Autowired
+    private ConsultaRepository repository;
+
+    public void validar(DadosAgendamentoConsulta dados){
+        var medicoPossuiConsultaNesseHorario = repository.existsByMedicoIdAndData(dados.idMedico(),dados.data());
+
+        if (medicoPossuiConsultaNesseHorario){
+            throw new ValidacaoException("Este médico já possui uma consulta nesse horário marcado!");
+        }
+    }
+}
