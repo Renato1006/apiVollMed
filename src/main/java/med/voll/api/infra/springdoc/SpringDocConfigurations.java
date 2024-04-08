b78a03d9c9aaf8f60b8c01e3bcbd5a39ccb76b2d
renato1006 <renato1006@gmail.com>
2024-04-08 20:40:22 -0300

    refactor: alterando o email de exemplo.

--- src/main/java/med/voll/api/infra/springdoc/SpringDocConfigurations.java
+++ src/main/java/med/voll/api/infra/springdoc/SpringDocConfigurations.java
@@ -26,7 +26,7 @@ public class SpringDocConfigurations {
                         .description("API Rest da aplicação Voll.med, contendo as funcionalidades de CRUD de médicos e de pacientes, além de agendamento e cancelamento de consultas")
                             .contact(new Contact()
                             .name("Time Backend Renato")
-                            .email("backend@voll.med"))
+                            .email("renato1006@gmail.com"))
                             .license(new License()
                                 .name("Apache 2.0")
                                 .url("http://voll.med/api/licenca")));
